ecc80df934e2bdcaf47118e5542c2d93e0ef719a
ramz <ramelopanisjr.06@gmail.com>
2025-07-13 22:43:53 +0800

    Update about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,17 +3,21 @@ import Navbar from "@/components/Navbar";
 import ZoomInOnLoad from "@/components/animate/ZoomInOnLoad";
 import Image from "next/image";
 import GalleryCards from "@/components/cards/GalleryCards";
-import { Amarante } from "next/font/google";
+import { Amarante, Lexend } from "next/font/google";
 
 const amarante = Amarante({ weight: "400", subsets: ["latin"] });
+const lexend = Lexend({
+  weight: ["100", "300", "400", "700"],
+  subsets: ["latin"],
+});
 export default function Index() {
   return (
-    <div className="w-full">
+    <div className={`w-full ${lexend.className}`}>
       <div className="mx-auto">
         {/* Header */}
         <Navbar />
         <div className="w-full flex items-center justify-center gap-5 px-10">
-          <div className="w-[500px] h-52 text-[18px] font-thin text-[#081b2b]">
+          <div className="w-[500px] h-52 text-[18px] font-light text-[#081b2b]">
             <p>I am a Front-End Developer.</p>
             <p>
               I enjoy exploring new technologies and environments that enhance
@@ -42,8 +46,32 @@ export default function Index() {
           <div>
             <p className={`${amarante.className} text-3xl`}>Gallery</p>
           </div>
-          <div className="w-[900px] overflow-x-auto">
+          <div className="w-[1000px] overflow-x-auto">
             <GalleryCards />
+          </div>
+        </div>
+        <div className="border-2 border-gray-300 bg-gray-100 shadow-md">
+          <div className="flex items-start p-10 justify-evenly">
+            <div className="flex flex-col gap-10">
+              <p className={`${amarante.className} text-3xl`}>
+                <span className="text-[#20b9f0]">P</span>art Designe
+                <span className="text-[#ff6347]">r</span>
+              </p>
+              <ul className="font-light text-lg">
+                <li>UI/UX design</li>
+                <li>Interaction design</li>
+              </ul>
+            </div>
+            <div></div>
+            <div className="flex flex-col gap-10">
+              <p className={`${amarante.className} text-3xl`}>Part Coder</p>
+              <ul className="font-light text-lg">
+                <li>Front-End Development</li>
+                <li>HTML / CSS / Tailwindcss</li>
+                <li>JavaScript</li>
+                <li>Curly Tops fav</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Lexend } from "next/font/google";
+import { Didact_Gothic } from "next/font/google";
 import Home from "./home";
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -9,13 +9,17 @@ import Home from "./home";
 //   variable: "--font-geist-mono",
 //   subsets: ["latin"],
 
-const lexend = Lexend({
+// const lexend = Lexend({
+//   weight: "400",
+//   subsets: ["latin"],
+// });
+const poppins = Didact_Gothic({
   weight: "400",
   subsets: ["latin"],
 });
 export default function Index() {
   return (
-    <div className={`${lexend.className} w-full min-h-screen`}>
+    <div className={`${poppins.className} w-full min-h-screen`}>
       <main className="w-full max-w-screen">
         <Home />
       </main>
