b2bdd8d3bfeb93938d2284e8015231cb1f6b1f2e
rams <ramelopanisjr.06@gmail.com>
2025-07-19 00:24:23 +0800

    Update: new portfolio navigate

--- src/components/LatestProjectsPage.tsx
+++ src/components/LatestProjectsPage.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image from "next/image";
 import React, { useState } from "react";
 import SlideUp from "./animate/SlideUp";
 import { ViewProjectsDialog } from "./ViewProjectsDialog";
+import { useRouter } from "next/navigation";
 
 export interface latestProjectTypes {
   projectName: string;
@@ -10,13 +12,13 @@ export interface latestProjectTypes {
   image: string;
 }
 const latestProject: latestProjectTypes[] = [
-  {
-    projectName: "New Portfolio!",
-    date: "2025-PRESENT",
-    description:
-      "Currently working on my new portofolio from Page Router to App Router",
-    image: "/assets/images/newportfolio.png",
-  },
+  // {
+  //   projectName: "New Portfolio!",
+  //   date: "2025-PRESENT",
+  //   description:
+  //     "Currently working on my new portofolio from Page Router to App Router",
+  //   image: "/assets/images/newportfolio.png",
+  // },
   {
     projectName: "DRSchecker - nextjs, gemini api",
     date: "2024-2025",
@@ -48,6 +50,7 @@ const latestProject: latestProjectTypes[] = [
 export default function LatestProjectsPage() {
   const [open, setOpen] = useState<boolean>(false);
   const [projectView, setProjectView] = useState<string>("");
+  const router = useRouter();
   return (
     <>
       <section
@@ -62,6 +65,33 @@ export default function LatestProjectsPage() {
           <div className="border border-slate-400 w-full"></div>
         </div>
         <div className="w-full flex flex-wrap items-center justify-center gap-6 text-[#081b2b]">
+          <SlideUp duration={0.5} delay={0.2}>
+            <div
+              onClick={() =>
+                router.push("https://rams-portfolio-next.vercel.app")
+              }
+              className="w-[430px] md:w-[330px] h-[340px] md:h-[270px] bg-gray-100 shadow-lg rounded-md flex flex-col items-center justify-center p-2.5 gap-2.5 hover:scale-105 transition-transform duration-300 cursor-pointer"
+            >
+              <div className="relative w-full h-full rounded-md">
+                <Image
+                  src={`/assets/images/newportfolio.png`}
+                  alt="Project Image"
+                  fill
+                  priority
+                  sizes="w-full h-full"
+                  className="object-cover rounded-md"
+                />
+              </div>
+              <div className="flex flex-col items-start justify-start w-full px-4">
+                <p className="text-[18px] font-thin">New Portfolio!</p>
+                <p className="font-thin text-[14px]">
+                  Currently working on my new portofolio from Page Router to Ap.
+                  . .
+                </p>
+                <p className="text-[10px]">2025-PRESENT</p>
+              </div>
+            </div>
+          </SlideUp>
           {latestProject.map((item, index) => (
             <SlideUp key={index} duration={0.5} delay={0.2}>
               <div
